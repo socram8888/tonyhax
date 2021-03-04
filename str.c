9036edbf6b747da954d8d4a73fe708a4c5effef5
Marcos Del Sol Vives <marcos@orca.pet>
2021-03-04 10:50:40 +0100

    Fix hex logging

--- str.c
+++ str.c
@@ -53,10 +53,12 @@ int mini_vsprintf(char * str, const char * format, va_list args) {
 					case 'X': {
 						char c;
 						uint32_t val = va_arg(args, uint32_t);
-						for (int i = 28; i >= 0; i--) {
-							c = '0' + (val >> i);
-							if (c > '9') {
-								c = c + 'A' - '9';
+						for (int i = 28; i >= 0; i -= 4) {
+							c = (val >> i) & 0xF;
+							if (c < 10) {
+								c += '0';
+							} else {
+								c += 'A' - 0xA;
 							}
 							*pos = c;
 							pos++;
