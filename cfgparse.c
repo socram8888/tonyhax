db3b358d35d48723c0f28944039f22c1201e65ea
Marcos Del Sol Vives <marcos@orca.pet>
2021-03-22 20:15:16 +0100

    Cleanup config parsing code

--- cfgparse.c
+++ cfgparse.c
@@ -29,83 +29,69 @@ const char * advance_until_start(const char * config) {
 	}
 }
 
-bool config_get_hex(const char * config, const char * wanted, uint32_t * value) {
+const char * find_wanted(const char * config, const char * wanted) {
 	uint32_t wanted_len = strlen(wanted);
 
-	while (true) {
-		// Check if first N characters match
-		if (strncmp(config, wanted, wanted_len) == 0) {
-			// Perfect, we're on the right line. Advance.
-			config += wanted_len;
+	// While first N characters don't match
+	while (strncmp(config, wanted, wanted_len) != 0) {
+		// No luck. Advance until next line.
+		config = strchr(config, '\n');
 
-			// Keep parsing until we hit the end of line or the end of file
-			uint32_t parsed = 0;
-			while (*config != '\n' && *config != '\0') {
-				uint32_t digit = todigit(*config);
-				if (digit < 0x10) {
-					parsed = parsed << 4 | digit;
-				}
-				config++;
-			}
-
-			// Save and return
-			*value = parsed;
-			return true;
-
-		} else {
-			// No luck. Advance until next line.
-			config = strchr(config, '\n');
-			
-			// If this is the last line, abort.
-			if (config == NULL) {
-				debug_write("Missing %s", wanted);
-				return false;
-			}
-
-			// Advance to skip line feed.
-			config++;
+		// If this is the last line, abort.
+		if (config == NULL) {
+			debug_write("Missing %s", wanted);
+			return NULL;
 		}
+
+		// Advance to skip line feed.
+		config++;
 	}
+
+	// Advance to after the name
+	return config + wanted_len;
+}
+
+bool config_get_hex(const char * config, const char * wanted, uint32_t * value) {
+	config = find_wanted(config, wanted);
+	if (!config) {
+		return false;
+	}
+
+	// Keep parsing until we hit the end of line or the end of file
+	uint32_t parsed = 0;
+	while (*config != '\n' && *config != '\0') {
+		uint32_t digit = todigit(*config);
+		if (digit < 0x10) {
+			parsed = parsed << 4 | digit;
+		}
+		config++;
+	}
+
+	// Save and return
+	*value = parsed;
+	return true;
 }
 
 bool config_get_string(const char * config, const char * wanted, char * value) {
-	uint32_t wanted_len = strlen(wanted);
-
-	while (true) {
-		// Check if first N characters match
-		if (strncmp(config, wanted, wanted_len) == 0) {
-			// Perfect, we're on the right line. Advance.
-			config += wanted_len;
-
-			// Advance until the start
-			while (*config == ' ' || *config == '=') {
-				config++;
-			}
-
-			// Copy until space or end of file
-			char * valuecur = value;
-			while (*config != '\0' && *config != '\n' && *config != '\r' && *config != ' ') {
-				*valuecur = *config;
-				config++;
-				valuecur++;
-			}
-
-			// Null terminate and return
-			*valuecur = '\0';
-			return true;
-
-		} else {
-			// No luck. Advance until next line.
-			config = strchr(config, '\n');
-			
-			// If this is the last line, abort.
-			if (config == NULL) {
-				debug_write("Missing %s", wanted);
-				return false;
-			}
-
-			// Advance to skip line feed.
-			config++;
-		}
+	config = find_wanted(config, wanted);
+	if (!config) {
+		return false;
 	}
+
+	// Advance until the start
+	while (*config == ' ' || *config == '=') {
+		config++;
+	}
+
+	// Copy until space or end of file
+	char * valuecur = value;
+	while (*config != '\0' && *config != '\n' && *config != '\r' && *config != ' ') {
+		*valuecur = *config;
+		config++;
+		valuecur++;
+	}
+
+	// Null terminate and return
+	*valuecur = '\0';
+	return true;
 }
