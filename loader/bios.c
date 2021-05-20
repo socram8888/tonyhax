0c86c69915d9c1109518886584655ef4781b8f7f
Marcos Del Sol Vives <marcos@orca.pet>
2021-05-21 00:24:37 +0200

    Initialize the boot configuration during the BIOS reinitialization

--- loader/bios.c
+++ loader/bios.c
@@ -7,12 +7,21 @@
 // Set to zero unless you are using an emulator or have a physical UART on the PS1, else it'll freeze
 const uint32_t tty_enabled = 0;
 
+// Address of A table
+uint32_t * const A0_TBL = (uint32_t *) 0x200;
+
 const char * const BIOS_DEVELOPER = (const char *) 0xBFC0012C;
 const char * const BIOS_VERSION = (const char *) 0xBFC7FF32;
 
 // Let's just pray these don't vary
 void ** const HANDLER_ARRAY = (void **) 0x100;
 uint32_t * const HANDLER_ARRAY_LEN = (uint32_t *) 0x104;
+
+struct boot_cnf_t {
+	uint32_t event;
+	uint32_t tcb;
+	uint32_t stacktop;
+};
 
 void initHandlersArray(int32_t priorities) {
 	// 8 is the size of the struct, whose contents we don't really care about
@@ -25,6 +34,24 @@ void initHandlersArray(int32_t priorities) {
 		*HANDLER_ARRAY = array;
 		*HANDLER_ARRAY_LEN = array_len;
 	}
+}
+
+struct boot_cnf_t * find_boot_cnf() {
+	/*
+	 * Extract from the "la t6, boot_cnf_values+8" opcodes at the start of the GetConf function.
+	 *
+	 * According to psx-spx docs this was used by Spec Ops Airborne Commando so it should work
+	 * on every retail BIOS.
+	 */
+	const uint8_t * get_conf_offset = (const uint8_t *) A0_TBL[0x9D];
+
+	// Extract high from the "lui"
+	uint16_t high = *((uint16_t *) (get_conf_offset + 0x00));
+
+	// Extract low from the "addi"
+	int16_t low = *((int16_t *) (get_conf_offset + 0x04));
+
+	return (struct boot_cnf_t *) (((int32_t) high << 16) + low - 8);
 }
 
 void bios_reinitialize() {
@@ -48,8 +75,8 @@ void bios_reinitialize() {
 		// Call it to restore everything that it needs to
 		((void (*)(void)) 0xA0000500)();
 
-		// Restore call tables
-		memcpy((uint8_t *)      0x200, (const uint8_t *) 0xBFC04300, 0x300);
+		// Restore A0 call table
+		memcpy((uint8_t *) A0_TBL, (const uint8_t *) 0xBFC04300, 0x300);
 	}
 
 	// Restore A, B and C tables
@@ -70,6 +97,17 @@ void bios_reinitialize() {
 
 	// Setup devices
 	InstallDevices(tty_enabled);
+
+	/*
+	 * Configure with default values
+	 *
+	 * We can't use SetConf as that one re-queues the CD events (and that's no bueno as
+	 * haven't initialized the system's memory yet).
+	 */
+	struct boot_cnf_t * boot_cnf = find_boot_cnf();
+	boot_cnf->event = 4;
+	boot_cnf->tcb = 16;
+	boot_cnf->stacktop = 0x801FFF00;
 
 	// Initialize kernel memory
 	SysInitMemory(0xA000E000, 0x2000);
